e79f155613b35573f61e3d2ec6d0c92ec3026b90
Daniel Jones <daniel.jones2000@gmail.com>
2019-09-30 13:19:41 -0700

    server running

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // implement your API here
+const express = require("express");
+
+const dp = require("./data/db");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+const port = 8000;
+server.listen(port, () => console.log("\nserver running\n"));
